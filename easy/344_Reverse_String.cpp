e944d3707799b9774cdf4f6f0d15ccd763034567
Ammar Sabit <ammarsabit39@gmail.com>
2025-06-23 10:10:51 +0300

    Reverse String [Easy]

---
+++ easy/344_Reverse_String.cpp
@@ -0,0 +1,13 @@
+#include <vector> 
+
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        for (int i; i < size(s)/2; i++){
+            char tmp[1];
+            tmp[0] = s[i];
+            s[i] = s[size(s) - (i+1)];
+            s[size(s) - (i+1)] = tmp[0];
+        }
+    }
+};
