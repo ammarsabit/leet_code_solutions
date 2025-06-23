312e2e3755822be9609bd97c98506196f901d87f
Ammar Sabit <ammarsabit39@gmail.com>
2025-06-23 13:35:12 +0300

    Solved  without converting the integer to a string

--- easy/9_Palindrome_Number.py
+++ easy/9_Palindrome_Number.py
@@ -1,3 +1,14 @@
 class Solution(object):
     def isPalindrome(self, x):
+        if x < 0 or (x % 10 == 0 and x != 0):
+            return False
+        y = x
+        z = 0
+        while y > 0:
+            z = (z*10) + (y%10) 
+            y //= 10
+        if z == x:
+            return True
+        return False
+
         
