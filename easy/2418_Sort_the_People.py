a32a59b96cc937bcd787684e06925fc15b0cf187
Ammar Sabit <ammarsabit39@gmail.com>
2025-06-24 08:01:21 +0300

    Optimized using list tupple instead of dictionary

--- easy/2418_Sort_the_People.py
+++ easy/2418_Sort_the_People.py
@@ -1,6 +1,6 @@
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
         people = [(heights[i], names[i]) for i in range(len(names))]
-        srted = [i[1] for i in sorted(people, reverse=True)] 
+        srted = [name for height, name in sorted(people, reverse=True)] 
     
         return srted
