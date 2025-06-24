620f0e41d0cab49f8a94834388f592848eb1597a
Ammar Sabit <ammarsabit39@gmail.com>
2025-06-24 07:55:47 +0300

    Optimization using tuple list instead of dict

--- easy/2418_Sort_the_People.py
+++ easy/2418_Sort_the_People.py
@@ -1,11 +1,6 @@
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
-        people = {heights[i]:names[i] for i in range(len(names))}
-        srted_height = reversed(sorted(heights))
-        srted_names = []
-
-        for i in srted_height:
-            srted_names.append(people[i])
-
-
-        return srted_names
+        people = [(heights[i], names[i]) for i in range(len(names))]
+        srted = [i[1] for i in sorted(people, reverse=True)] 
+    
+        return srted
