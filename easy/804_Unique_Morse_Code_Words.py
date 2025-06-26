f083bccfbbbf3cefb988b79ffb72a7ae7d3b79aa
Ammar Sabit <ammarsabit39@gmail.com>
2025-06-26 14:57:03 +0300

    Solved

---
+++ easy/804_Unique_Morse_Code_Words.py
@@ -0,0 +1,17 @@
+class Solution:
+    def uniqueMorseRepresentations(self, words: List[str]) -> int:
+        morse = [".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
+        ascii = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+        pair = dict(zip(ascii, morse))
+        transformations = set()
+
+        for word in words:
+            morse_version = ''
+            for s in word:
+                morse_version += pair[s]
+
+            transformations.add(morse_version)
+            morse_version = ''
+
+        return len(transformations)          
